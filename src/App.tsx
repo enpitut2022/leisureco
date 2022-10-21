1f0283b510fccd189e272411fc398259838647b8
monokemonoke <kawabataryuhei@gmail.com>
2022-10-21 17:08:38 +0900

    fix: ts-ignoreを許容

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import './App.css'
-import TinderCard from 'react-tinder-card'
+import Card from './pages/cards'
 
 function App() {
   return (
     <div className="App">
-      <TinderCard>
-        <div className='shop-card'>
-          <p>つくば温泉 喜楽里 別邸</p>
-          <p>09:00 ~ 24:00</p>
-          <img src="https://www.yurakirari.com/kirari/tsukuba/images/inside_spa/inside_spa_ph01.jpg"></img>
-          <p>1000 ~ 2000円</p>
-        </div>
-      </TinderCard>
+      <Card />
     </div>
   )
 }
