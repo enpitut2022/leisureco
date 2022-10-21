a0d48065946b583d2ac26c8363d6a38baf73457c
monokemonoke <kawabataryuhei@gmail.com>
2022-10-21 17:02:14 +0900

    fix: 店のカードがスワイプできるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import TinderCard from 'react-tinder-card'
 function App() {
   return (
     <div className="App">
-      <TinderCard>hoge</TinderCard>
-      <div className='shop-card'>
-        <p>つくば温泉 喜楽里 別邸</p>
-        <p>09:00 ~ 24:00</p>
-        <img src="https://www.yurakirari.com/kirari/tsukuba/images/inside_spa/inside_spa_ph01.jpg"></img>
-        <p>1000 ~ 2000円</p>
-      </div>
+      <TinderCard>
+        <div className='shop-card'>
+          <p>つくば温泉 喜楽里 別邸</p>
+          <p>09:00 ~ 24:00</p>
+          <img src="https://www.yurakirari.com/kirari/tsukuba/images/inside_spa/inside_spa_ph01.jpg"></img>
+          <p>1000 ~ 2000円</p>
+        </div>
+      </TinderCard>
     </div>
   )
 }
