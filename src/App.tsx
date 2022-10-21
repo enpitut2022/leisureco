328c3e5d17a301aaa28e7f3d422ac798afecafd9
monokemonoke <kawabataryuhei@gmail.com>
2022-10-21 17:00:07 +0900

    add: カードを動かすことができた

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import TinderCard from 'react-tinder-card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <TinderCard>hoge</TinderCard>
       <div className='shop-card'>
         <p>つくば温泉 喜楽里 別邸</p>
         <p>09:00 ~ 24:00</p>
